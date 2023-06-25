1e56431081e916ba5075f36144bf22afd620ba58
BorisKlco <klco@hotmail.sk>
2023-06-25 19:00:53 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react.ToDoList
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-tlpzk4)
---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+function App() {
+  const [task, setTask] = useState('');
+  const [taskList, setList] = useState([]);
+
+  const handleTask = (e) => {
+    setTask(e.target.value);
+  };
+
+  const addTask = () => {
+    setList((taskList) => [...taskList, task]);
+  };
+
+  const removeTask = (taskIndex) => {
+    setList((taskList) => taskList.filter((_, index) => index !== taskIndex));
+  };
+
+  return (
+    <>
+      <h1>react.ToDoList</h1>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <label>
+          To Do: <input onChange={handleTask} value={task} />
+        </label>
+        <button onClick={addTask}>Add</button>
+      </form>
+      <p>useState: {task}</p>
+      <ol>
+        {taskList.map((task, index) => (
+          <>
+            <li key={index}>
+              {task} <button onClick={() => removeTask(index)}>X</button>
+            </li>
+          </>
+        ))}
+      </ol>
+    </>
+  );
+}
+
+export default App;
